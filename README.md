1fa7fbfefcfb78159f790fabed2be0b92360bbde
Falon Darville <falondarville@gmail.com>
2018-01-24 20:42:49 -0800

    README and pseudocode

---
+++ README.md
@@ -0,0 +1,6 @@
+How to play Hangman:
+1. Press any key to begin
+2. Type letters; these are your guesses.
+		If the guess is correct, the letter will appear in its appropriate spot on the game. If the guess is wrong, you will lose a chance. 
+3. Continue guessing until you either run out of guesses or 
+you have guessed the whole word correctly.
---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// trigger first hangman when the player presses any key 
+
+// count the number of wins that a play has during the session
+
+// count the number of guesses remaining
+
+// list the letters already guessed
+
+// populate the correctly guessed letters on the hangman game
+
+// trigger choosing another word when the play wins or loses the game
