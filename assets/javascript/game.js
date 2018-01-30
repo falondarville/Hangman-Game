d838a40c424f16d5249f88f7a240d1fb69d256fe
Falon Darville <falondarville@gmail.com>
2018-01-29 20:43:21 -0800

    add statistics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,71 +7,61 @@ let lettersUsed = [];
 let lettersGuess = lettersUsed.length;
 let remainingLetters = currentWord.length;
 
-	// Keeps track of letters used and manages change of array
+	// 1. Generates blank wordArray based on random word picked by computer
+	// 2. Adds guessed letters to blank array spaces
+	// 3. Print game statistics to user interface
+	// 4. Adds wins
 
 	updateLetters = function() {
+		let correctLetters = 0;
 		let wordArray = [];
 			for (var i = 0; i < currentWord.length; i++) {
 				wordArray[i] = "  ___  ";
 			if (lettersUsed.indexOf(currentWord[i]) > -1) {
-				console.log(lettersUsed.join());
 				wordArray[i] = currentWord[i];
+				correctLetters++;
 			}
+		}
+		if (correctLetters === currentWord.length) {
+			currentWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+			lettersUsed = [];
+			wordArray = [];
+			guessesRemaining = 15;
+			wins++;
+			document.getElementById("wordAppear").innerHTML = wordArray.join("");
+			updateLetters();
 		}
 
 		document.getElementById("wordAppear").innerHTML = wordArray.join("");
 		document.getElementById("lettersGuessed").innerHTML = lettersUsed;
-		// document.getElementById("guessesRemaining").innerHTML = // code this
-		// document.getElementById("wins").innerHTML = // code this
+		document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+		document.getElementById("wins").innerHTML = wins;
 	}
-
-	//once all letters are guessed correctly, wins counter goes up by one
 
 	//TESTING
 
 updateLetters();
 console.log(currentWord);
 
-	// START GAME: trigger first hangman when the player presses any key 
+	// 1. Triggers start of the game when user presses any key
+	// 2. Resets lettersUsed and guessesRemaining when player has guessed max letters (15)
+	// 3 Calculates guessesRemaining
+
+// PROBLEM game reset is not automatically updating after 15 guesses or win. onkeyup applies. 
 
 	document.onkeyup = function(event) {
 		if (lettersGuess === 15) {
 			currentWord = wordBank[Math.floor(Math.random()*wordBank.length)];
 			lettersUsed = [];
 			guessesRemaining = 15;	
-			alert("max guesses");
+			document.getElementById("") // add a section with text notifying player of reset
 		} else {
 			let letter = String.fromCharCode(event.keyCode).toLowerCase();
 			if (lettersUsed.indexOf(letter) == -1) {
 				lettersUsed.push(letter);
+				guessesRemaining--; //decrements guessesRemaining by one each time
 			}
 			lettersGuess = lettersUsed.length;
 		}
 		updateLetters();
 	}
-
-// populate the correctly guessed letters on the hangman game
-	//put letters in correct spot locations
-
-// count the number of wins that a play has during the session
-
-	//start at 0 and add one per time that a user completes the puzzle before the 
-	//max number of guesses is over
-
-// count the number of guesses remaining
-
-	//start at 15 and then decrease by one with each guess
-	//decreased regardless of whether or not the guess was correct
-
-// list the letters already guessed
-
-	//list all letters that player types on keyboard
-	//include letters guessed correctly
-
-// trigger choosing another word when the play wins or loses the game
-
-	//if
-		//a. max number of guess = 0 and puzzle is not solved
-			//then trigger new word
-		//b. puzzle is solved
-			//then trigger new word AND add 1 to wins
